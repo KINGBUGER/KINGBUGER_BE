86bbd8f44dcf96ce54a984a55e3ca2331befd146
kim0705 <dbwls138@naver.com>
2024-11-30 20:58:15 +0900

    백엔드 초기 세팅

---
+++ src/test/java/org/example/project_back/ProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.project_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
